e25c5e2de2bd771a1b94823fe8ddb68c44187e14
Tan <chengyi.tan@accenture.com>
2018-01-31 17:52:26 +0800

    Import EPPlus by Nuget:Install-Package EPPlus -Version 4.1.1

---
+++ AutomationExcelOperation/AutomationExcelOperation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationExcelOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo outputDir = new DirectoryInfo(@"C:\");
+            if (!outputDir.Exists) throw new Exception("Output Directory doesn't exists.");
+            string output = "";
+
+            FileInfo testDataExcelFile = new FileInfo("..\\..\\Data\\RawExcel\\TestData.xlsx");
+            if (!testDataExcelFile.Exists) throw new Exception("TestData excel file doesn't exist.");
+        }
+    }
+}
